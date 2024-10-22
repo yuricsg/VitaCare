4f6d8b4e45c30af5f87a27500344fd9f92e32b39
Yuri França <yuriicardo004@gmail.com>
2024-10-21 21:23:06 -0300

    Mensagem de erro ao cadastrar

--- app_vitacare/views.py
+++ app_vitacare/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render, redirect
 from .forms import UsuarioForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from .models import Usuario
 
 def index(request):
     return render(request,'usuarios/index.html')
 
 def user_login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')  # Redireciona após login bem-sucedido
+        else:
+            messages.error(request, 'Email ou senha incorretos.')  # Mostra mensagem de erro
+            return redirect('login')  # Volta para a página de login
+
     return render(request, 'usuarios/login.html')
 
 def register(request):
@@ -12,13 +27,13 @@ def register(request):
         form = UsuarioForm(request.POST)
         if form.is_valid():
             usuario = form.save(commit=False)
-            usuario.senha = form.cleaned_data['senha']  # Aqui você pode adicionar criptografia da senha, se desejar.
+            usuario.set_password(form.cleaned_data['senha'])  
             usuario.save()
             return redirect('login')  
         else:
             form = UsuarioForm()
-        return render(request, 'usuarios/register.html', {'form': form})
 
+        return render(request, 'usuarios/register.html', {'form': form})
 
 def consultas(request):
   return render(request, 'usuarios/consultas.html')
